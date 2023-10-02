afb366a7601224e7b911578747bfe13d29125230
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 11:08:58 +0200

    test of SQL export

--- ConnectionPoint/schema.sql
+++ ConnectionPoint/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model ConnectionPoint of the subject dataModel.S4SYST for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model ConnectionPoint of the subject dataModel.S4SYST for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE ConnectionPoint_type AS ENUM ('ConnectionPoint');
-CREATE TABLE ConnectionPoint (address json, alternateName text, areaServed text, connectionPointOf json, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type ConnectionPoint_type);
+CREATE TABLE ConnectionPoint (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type ConnectionPoint_type);
