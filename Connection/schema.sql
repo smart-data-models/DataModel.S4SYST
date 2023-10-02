22f3a5ddba280fc3b4d9a3c4dbbbaed59722b901
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 11:08:55 +0200

    test of SQL export

--- Connection/schema.sql
+++ Connection/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model Connection of the subject dataModel.S4SYST for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model Connection of the subject dataModel.S4SYST for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Connection_type AS ENUM ('Connection');
-CREATE TABLE Connection (address json, alternateName text, areaServed text, connectsSystem json, connectsSystemAt json, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, seeAlso json, source text, type Connection_type);
+CREATE TABLE Connection (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, source TEXT, type Connection_type);
