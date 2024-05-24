6870b650b071248c9eb4a4ee24a7e85f610342e1
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 18:30:20 +0200

    created example of code

--- ConnectionPoint/code/code_for_using_dataModel.S4SYST_ConnectionPoint.py
+++ ConnectionPoint/code/code_for_using_dataModel.S4SYST_ConnectionPoint.py
@@ -24,13 +24,18 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "ConnectionPoint"
 subject = "dataModel.S4SYST"
-connectionPointOf = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:ConnectionPoint:85945672-ee4a-47db-9081-163a2b78bf11'}"
+connectionPointOf = "urn:ngsi-ld:ConnectionPoint:7e2ece1a-1826-4d62-99a3-cbbcbbb09952"
 attribute = "connectionPointOf"
 value = connectionPointOf
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
